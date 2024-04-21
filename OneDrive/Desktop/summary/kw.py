a3bcbcae7bb4bac381dd77178184b8c69451ce9c
AthulyaprasannakumarMC <115775602+AthulyaprasannakumarMC@users.noreply.github.com>
2024-04-21 21:17:48 +0530

    Add files via upload

---
+++ OneDrive/Desktop/summary/kw.py
@@ -0,0 +1,39 @@
+# import pyttsx3
+
+# def text_to_audio(text):
+    # Initialize the TTS engine
+    # engine = pyttsx3.init()
+
+    # Set properties for the TTS engine
+    # engine.setProperty('rate', 150)  # Speed of speech
+    # engine.setProperty('volume', 1)   # Volume (0.0 to 1.0)
+
+    # Convert the text to speech
+    # engine.say(text)
+
+    # Play the speech
+    # engine.runAndWait()
+
+# Example text
+# text = "L i y a Antony Software Engineer."
+
+# Convert text to audio and play it
+# text_to_audio(text)
+
+
+
+
+# from googletrans import Translator
+
+# # Initialize the translator
+# translator = Translator()
+
+# # Text to be translated
+# text_to_translate = "Hello, how are you?"
+
+# # Translate the text from English to Spanish
+# translated_text = translator.translate(text_to_translate, src='en', dest='ml')
+
+# # Print the translated text
+# print("Original Text:", text_to_translate)
+# print("Translated Text:", translated_text.text)
